b1c628bb7f684fedfdc47a16a9d1143b08bd3cd4
yulia <yuliarodrigues@aluno.unifapce.edu.br>
2026-01-30 17:54:49 -0300

    removendo erros

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+
 
 # --- Configuração da Página ---
 # Define o título da página, o ícone e o layout para ocupar a largura inteira.
